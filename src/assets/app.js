d969ceb10c2c059a56b2265b9e580724141c8ee4
rodrigoaiz <rodrigoaiz@gmail.com>
2025-02-20 10:08:54 -0600

    update app.js to import styles and modify logo.svg for improved design

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,3 +1,3 @@
 // Archivo vacío por ahora o agrega cualquier lógica JS necesaria
-//import './css/styles.css';
+import './css/styles.css'; // Ajusta la ruta según sea necesario
 console.log('App cargada');
