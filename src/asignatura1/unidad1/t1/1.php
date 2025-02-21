2438e9d425fd9f155099d0de6a47ebd1c5b86d31
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2025-02-21 15:19:05 -0600

    dynamically set menu path based on subject name from URL

--- src/asignatura1/unidad1/t1/1.php
+++ src/asignatura1/unidad1/t1/1.php
@@ -1,8 +1,12 @@
 <?php
 include '../../../config.php';
 include PATH_INCLUDE . 'TemplatePages.php';
+// Obtener el nombre de la asignatura desde la URL
+$urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pathSegments = explode('/', trim($urlPath, '/'));
+$asignaturaName = $pathSegments[1]; // Ajusta el índice según la estructura de tu URL
 // Ruta al archivo JSON específico de la asignatura
-$menuAsignaturaPath = BASE_PATH . '/asignatura1/assets/data/menu_asignatura.json';
+$menuAsignaturaPath = BASE_PATH . '/' . $asignaturaName . '/assets/data/menu_asignatura.json';
 ob_start();
 ?>
 asd
