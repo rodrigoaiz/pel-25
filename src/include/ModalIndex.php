56d4d65c7c13ded1d309e3643c0f236bec3260fd
rodrigoaiz <rodrigoaiz@gmail.com>
2025-03-11 11:00:42 -0600

    fix: remove tabindex attribute from modal and clean up close button markup

--- src/include/ModalIndex.php
+++ src/include/ModalIndex.php
@@ -7,7 +7,7 @@ function renderModalIndex($ModalContent2)
 
   ob_start();
 ?>
-  <div id="default-modal" tabindex="-1" aria-hidden="true" class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full bg-black/63 backdrop-blur-sm">
+  <div id="default-modal" class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full bg-black/63 backdrop-blur-sm">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow-sm">
@@ -16,7 +16,7 @@ function renderModalIndex($ModalContent2)
           <h3 class="text-xl font-semibold text-gray-900">
             <?php echo htmlspecialchars($ModalTittle); ?>
           </h3>
-          <button class="close-modal-button cursor-pointer text-gray-700 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+          <button class="close-modal-button cursor-pointer text-gray-700 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
             </svg>
