48e7021f978904c87ac1b8a9772ffbdd4b744ebf
rodrigoaiz <rodrigoaiz@gmail.com>
2025-04-11 14:18:39 -0600

    feat: enhance renderImage function with optional shadow and custom classess

--- src/include/ImagenPie.php
+++ src/include/ImagenPie.php
@@ -1,7 +1,11 @@
 <?php
-  function renderImage($img, $piedefoto = null, $link = '#', $textolink = '')
+  function renderImage($img, $piedefoto = null, $link = '#', $textolink = '', $useShadow = false, $imgClass = '')
   {
-    echo '<img class="my-0 min-w-full" src="' . PATH_IMAGES . $img . '" alt="'.$piedefoto.'" />';
+    // Clases base con sombra opcional y clases adicionales
+    $class = 'my-0 min-w-full' . ($useShadow ? ' shadow-lg' : '') . (!empty($imgClass) ? ' ' . $imgClass : '');
+
+    // Renderizar la imagen y el pie de foto
+    echo '<img class="' . $class . '" src="' . PATH_IMAGES . $img . '" alt="' . $piedefoto . '" />';
     echo '<p class="text-center text-sm mt-2 mb-3">' . $piedefoto .
          ($link && $textolink ? ' <a href="' . $link . '" target="_blank" rel="noreferrer">' . $textolink . '</a>' : '') .
          '</p>';
