ea5dc630bed1e1502a686e6811228a75f59a9782
David Torres Ortuño <holadavidortuno@gmail.com>
2025-05-19 14:49:37 -0600

    Cambio en config

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
   define('CONFIG_LOADED', true);
   define('BASE_PATH', __DIR__);
-  define('BASE_URL', '/modelo-educativo/dist/');
-  //define('BASE_URL', '/dist');
+  //define('BASE_URL', '/modelo-educativo/dist/');
+  define('BASE_URL', '/dist');
   define('ASSET_URL', BASE_URL . '/assets/');
   define('PATH_ICONS', BASE_URL . '/assets/icons/');
   define('PATH_INCLUDE', BASE_PATH . '/include/');
