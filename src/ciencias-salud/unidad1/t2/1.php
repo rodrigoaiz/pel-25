915300682c4fe87a61b22a5921f42b4223717a1e
rodrigoaiz <rodrigoaiz@gmail.com>
2025-09-04 13:37:00 -0600

    u1t2p1 avance

--- src/ciencias-salud/unidad1/t2/1.php
+++ src/ciencias-salud/unidad1/t2/1.php
@@ -3,6 +3,8 @@ include '../../../config.php';
 include PATH_INCLUDE . 'TemplatePages.php';
 include PATH_INCLUDE . 'ImagenPie.php';
 include PATH_INCLUDE . 'Tabs.php';
+include PATH_INCLUDE . 'ImagenFullPleca.php';
+include PATH_INCLUDE . 'ActividadIframe.php';
 
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $menuAsignaturaPath = getMenuAsignaturaPath($urlPath);
@@ -36,7 +38,24 @@ ob_start();
         'Involucran cómo el individuo interactúa con su entorno social, por ejemplo, las oportunidades educativas, la pobreza, el desempleo, las relaciones interpersonales, las leyes, las normas sociales, y otros factores socioeconómicos que pueden influir en el bienestar y salud de las personas.'
       ],
     ],
-  ]) ?>
+  ])
+  ?>
+  <?php
+  renderImagePleca('u1-diagrama-factores.png', 'Modelo biopsicosocial.');
+  ?>
+  <?php ob_start(); ?>
+  <p>Después de revisar el tema de: Identifica al ser humano como unidad biopsicosocial, participa en el foro con tus compañeros, iniciando una discusión a partir de las siguientes preguntas generadoras. Recuerda que debes ser respetuoso de las opiniones de los demás, aun cuando no estés de acuerdo con ellas.</p>
+  <h4>Preguntas generadoras</h4>
+  <p>Desde tu punto de vista</p>
+  <ul>
+    <li>¿ Qué factores influyen en la salud del ser humano?</li>
+    <li>¿ Por qué el hombre es considerado como un ser biopsicosocial?</li>
+    <li>Comenta la participación de algunos de tus compañeros.</li>
+  </ul>
+  <?php
+  $ActividadContent = ob_get_clean();
+  renderActividad('u1t1a2', "Foro de discusión  ", $ActividadContent);
+  ?>
 </section>
 <?php
 $content = ob_get_clean();
