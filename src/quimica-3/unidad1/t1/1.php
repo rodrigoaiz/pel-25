24e8d0746ed9f30fefe75bb69c3574f868b2bc00
Karina Lisset Lavadores Pérez <karinalissetlavadores323@alumno.cch.unam.mx>
2025-09-12 21:39:48 -0600

    unidad 1 terminada

--- src/quimica-3/unidad1/t1/1.php
+++ src/quimica-3/unidad1/t1/1.php
@@ -3,6 +3,8 @@ include '../../../config.php';
 include PATH_INCLUDE . 'TemplatePages.php';
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $menuAsignaturaPath = getMenuAsignaturaPath($urlPath);
+include PATH_INCLUDE . 'ActividadIframe.php';
+include PATH_INCLUDE . 'Videos.php';
 ob_start();
 ?>
 <section>
@@ -11,7 +13,7 @@ ob_start();
   <p>Esta unidad contiene tres aprendizajes en los que se desarrollarán diferentes habilidades cognitivas a partir de temáticas vinculadas con la Industria química en México. Es importante que reconozcas que la temática de la asignatura está presente en diferentes ámbitos de la vida cotidiana y, en este caso, existe una relación con la industria y la productividad en la economía mexicana.</p>
   
   <?php
-  renderVideoIframe('<iframe src="https://drive.google.com/file/d/1A7JWWA_9J8t_e6yAOwMMa3hpg5iZMikF/preview" width="640" height="480" allow="autoplay"></iframe>', 'Este es el primer video de la asignatura');
+  renderVideoIframe('xvBzzbl6_2w', 'Este es el primer video de la asignatura');
   ?>
 
   <p>Para comenzar, resolverás un cuestionario diagnóstico de opción múltiple, que tiene como finalidad indagar sobre los conocimientos generales que tienes de las asignaturas de Química que ya cursaste. ¡Adelante! </p>
