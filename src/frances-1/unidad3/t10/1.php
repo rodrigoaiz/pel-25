403955cd6f02844ac233203ad54ba1f8137d6b59
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2026-06-22 13:55:44 -0600

    fix en examen final frances 1

--- src/frances-1/unidad3/t10/1.php
+++ src/frances-1/unidad3/t10/1.php
@@ -8,14 +8,13 @@ $menuAsignaturaPath = getMenuAsignaturaPath($urlPath);
 ob_start();
 ?>
 <section>
-    <h2>Examen final</h2>
-    <h3>Examen final</h3>
+    <h2>Évaluation finale de l'unité</h2>
     <p>Félicitations! Tu as terminé la matière de Français I. Réalise l’activité d’évaluation suivante, qui te permet de mesurer l’acquis de ton apprentissage dans cette matière. / ¡Felicidades! Has concluído la asignatura de Francés I. Realiza la siguiente actividad de evaluación, que te permite conocer el logro de tu aprendizaje en esta asignatura.</p>
 
     <?php ob_start(); ?>
     <?php
     $ActividadContent = ob_get_clean();
-    renderActividad('u3t9a20', "Examen final", $ActividadContent);
+    renderActividad('u3a3', "Évaluation finale de l'unité", $ActividadContent);
     ?>
 </section>
 <?php
